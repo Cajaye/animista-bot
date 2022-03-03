7da349e3c29c5953f4e87ee73d1d10bfdaf39e7a
Cajaye Clarke <cajayeclarke44@gmail.com>
2022-03-03 15:32:58 -0500

    fix

--- server.js
+++ server.js
@@ -13,10 +13,18 @@ process.on('unhandledRejection', (reason, promise) => {
 })
 
 require('dotenv').config();
+const fs = require("fs");
 const { Client, Intents, Collection } = require("discord.js");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
 
 client.once('ready', () => {
     console.log('Ready!');
